21faf8d90e4dd1dc64f03a48ce231c55748124cc
guozhaokui <guozhaokuiwang@gmail.com>
2021-10-18 13:23:42 +0800

    temp add fbx

---
+++ wasm/fbx.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+mkdir -p build/AssetLib/FBX
+pushd build/AssetLib/FBX
+
+clang++ \
+    -c \
+    --target=wasm32 -nostdlib -O3 -DWEBASM -DBT_USE_DOUBLE_PRECISION -D__wasi__ -DNDEBUG -DNO_OPENGL3 \
+    -fvisibility=hidden \
+    -mthread-model single \
+    -fno-threadsafe-statics \
+    -fno-exceptions \
+    -std=c++11 \
+    -I../../../../code/AssetLib/FBX \
+    -I../../../../include \
+    -Wall \
+    --sysroot=${WASI_SDK_HOME}/share/wasi-sysroot \
+    ../../../../code/AssetLib/FBX/*.cpp \
+    
+
+llvm-ar \
+    rc ./fbx.a \
+    *.o
+
+popd
+
+
