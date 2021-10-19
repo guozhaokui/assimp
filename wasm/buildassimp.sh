47073ac500a92a2abb370d6ffd362d5f8cb09850
guozhaokui <guozhaokuiwang@gmail.com>
2021-10-19 12:43:20 +0800

    不需要.imports文件了，也不需要在lld的时候指定导出函数

--- wasm/buildassimp.sh
+++ wasm/buildassimp.sh
@@ -4,7 +4,7 @@ mkdir -p build
 pushd build
 
 clang++ -c\
-    --target=wasm32 -nostdlib -O3 -DWEBASM -DBT_USE_DOUBLE_PRECISION -D__wasi__ -DNDEBUG -DNO_OPENGL3 \
+    --target=wasm32 -nostdlib -O3 -DWEBASM -DBT_USE_DOUBLE_PRECISION -D__wasi__ -DNDEBUG  \
     -mthread-model single \
     -fno-threadsafe-statics \
     -fvisibility=hidden \
@@ -15,22 +15,19 @@ clang++ -c\
     --sysroot=${WASI_SDK_HOME}/share/wasi-sysroot \
     ../test.cpp \
     
-#jslib.imports 中的符号在运行时连接
 # --strip-all 会去掉符号
 #    --strip-debug \ -S
 #    --compress-relocations \
 
 wasm-ld        \
     --lto-O3 --no-entry\
-    --allow-undefined-file=../jslib.imports \
     --import-memory \
-    --export=test \
     --export-dynamic \
     ../build/AssetLib/FBX/fbx.a \
     ../build/Common/Common.a \
     ../build/PostProcessing/PostProcessing.a \
     *.o\
-    -o bullet.wasm
+    -o assimp.wasm
 
 popd
 
--- wasm/test.cpp
+++ wasm/test.cpp
@@ -1,6 +1,6 @@
-#include <assimp/cimport.h>        // Plain-C interface
-#include <assimp/scene.h>          // Output data structure
-#include <assimp/postprocess.h>    // Post processing flags
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
 
 #define WASM_EXP __attribute__((visibility("default")))
 #define __BTWASM_SYSCALL_NAME(name) \
@@ -8,32 +8,42 @@
 
 
 bool DoTheImportThing( const char* pFile) {
-// Start the import on the given file with some example postprocessing
-// Usually - if speed is not the most important aspect for you - you'll t
-// probably to request more postprocessing than we do in this example.
-const struct aiScene* scene = aiImportFile( pFile,
+  // Create an instance of the Importer class
+/*
+  Assimp::Importer importer;
+
+  // And have it read the given file with some example postprocessing
+  // Usually - if speed is not the most important aspect for you - you'll
+  // probably to request more postprocessing than we do in this example.
+  const aiScene* scene = importer.ReadFile( pFile,
     aiProcess_CalcTangentSpace       |
     aiProcess_Triangulate            |
     aiProcess_JoinIdenticalVertices  |
     aiProcess_SortByPType);
 
-// If the import failed, report it
-if( NULL != scene) {
+  // If the import failed, report it
+  if (nullptr != scene) {
+    //DoTheErrorLogging( importer.GetErrorString());
     return false;
-}
+  }
 
-// Now we can access the file's contents
-//DoTheSceneProcessing( scene);
-
-// We're done. Release all resources associated with this import
-aiReleaseImport( scene);
-return true;
+  // Now we can access the file's contents.
+  //DoTheSceneProcessing( scene);
+*/
+  // We're done. Everything will be cleaned up by the importer destructor
+  return true;
 }
 
 
 extern "C"{
+	// 导入函数
+	void jslogs(const char* str) __BTWASM_SYSCALL_NAME(logs);
+	void jslog(const char* str, int len, float f1, float f2, float f3) __BTWASM_SYSCALL_NAME(log);
+
 	void WASM_EXP test(){
         DoTheImportThing("");
+        jslogs("a");
 	}
+
 }
 
