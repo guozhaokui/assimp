89db2f4ad8d81948a9119b6247d950178612b5ff
guozhaokui <guozhaokuiwang@gmail.com>
2021-10-23 20:15:24 +0800

    -g --> O1 否则无法跟踪代码

--- wasm/buildassimp.sh
+++ wasm/buildassimp.sh
@@ -5,7 +5,7 @@ pushd build
 
 clang++ -c\
     --sysroot ${WASI_SDK_HOME}/share/wasi-sysroot \
-    --target=wasm32 -nostdlib -g -DWEBASM -DBT_USE_DOUBLE_PRECISION -D__wasi__ -DNDEBUG  \
+    --target=wasm32 -nostdlib -O1 -DWEBASM -DBT_USE_DOUBLE_PRECISION -D__wasi__ -DNDEBUG  \
     -mthread-model single \
     -fno-threadsafe-statics \
     -fvisibility=hidden \
--- wasm/test.cpp
+++ wasm/test.cpp
@@ -1,13 +1,12 @@
-#include <assimp/Importer.hpp>      // C++ importer interface
-#include <assimp/scene.h>           // Output data structure
-#include <assimp/postprocess.h>     // Post processing flags
+#include <assimp/postprocess.h> // Post processing flags
+#include <assimp/scene.h> // Output data structure
 #include <assimp/IOStream.hpp>
 #include <assimp/IOSystem.hpp>
-
+#include <assimp/Importer.hpp> // C++ importer interface
 
 #define WASM_EXP __attribute__((visibility("default")))
 #define __BTWASM_SYSCALL_NAME(name) \
-	__attribute__((__import_module__("BTJSRT"), __import_name__(#name)))
+    __attribute__((__import_module__("BTJSRT"), __import_name__(#name)))
 
 bool DoTheImportThing(const char *pFile);
 
@@ -16,79 +15,121 @@ extern "C" {
 void jslogs(const char *str) __BTWASM_SYSCALL_NAME(logs);
 void jslog(const char *str, int len, float f1, float f2, float f3) __BTWASM_SYSCALL_NAME(log);
 
+void jsLoadFile(const char* f) __BTWASM_SYSCALL_NAME(loadFile);;
+
 void WASM_EXP test() {
-    DoTheImportThing("");
-    jslogs("a");
+    DoTheImportThing("/test/tt.fbx");
+}
+
+void* _malloc(int sz) {
+    return malloc(sz);
+}
+
+void _delete(void* ptr) {
+    delete ptr;
 }
 }
 
 // My own implementation of IOStream
 class MyIOStream : public Assimp::IOStream {
-  friend class MyIOSystem;
+    friend class MyIOSystem;
 
+public:    
+    std::string file;
+    char* pBuffer;
+    int length;
 protected:
-  // Constructor protected for private usage by MyIOSystem
-  MyIOStream();
+    // Constructor protected for private usage by MyIOSystem
+    MyIOStream(const char* pf) {
+      file.assign(pf);
+      jsLoadFile(pf);
+    }
 
 public:
-  ~MyIOStream();
-  size_t Read( void* pvBuffer, size_t pSize, size_t pCount) {  }
-  size_t Write( const void* pvBuffer, size_t pSize, size_t pCount) {  }
-  aiReturn Seek( size_t pOffset, aiOrigin pOrigin) {  }
-  size_t Tell() const {  }
-  size_t FileSize() const {  }
-  void Flush () {  }
+    virtual ~MyIOStream() {
+    }
+
+    // psize 是每个元素的大小。pcount是多少个元素
+    size_t Read(void *pvBuffer, size_t pSize, size_t pCount) {
+      jslogs((char*)pvBuffer);
+      return 0;
+    }
+
+    size_t Write(const void *pvBuffer, size_t pSize, size_t pCount) {
+    }
+
+    aiReturn Seek(size_t pOffset, aiOrigin pOrigin) {
+      jslogs((char*)555);
+    }
+
+    size_t Tell() const {
+      jslogs((char*)333);
+    }
+    size_t FileSize() const {
+      jslogs((char*)444);
+    }
+
+    void Flush() {
+    }
 };
 
 // Fisher Price - My First Filesystem
 class MyIOSystem : public Assimp::IOSystem {
-  MyIOSystem() {  }
-  ~MyIOSystem() {  }
+public:
+    MyIOSystem() {
+    }
+    virtual ~MyIOSystem() {
+    }
 
-  // Check whether a specific file exists
-  bool Exists( const std::string& pFile) const {
-  }
+    // Check whether a specific file exists
+    virtual bool Exists(const char *pFile) const {
+        jslogs((char *)22);
+        return true;
+    }
 
-  // Get the path delimiter character we'd like to see
-  char GetOsSeparator() const {
-    return '/';
-  }
+    // Get the path delimiter character we'd like to see
+    virtual char getOsSeparator() const {
+        return '/';
+    }
 
-  //  and finally a method to open a custom stream
-  Assimp::IOStream* Open( const std::string& pFile, const std::string& pMode) {
-    return new MyIOStream(  );
-  }
+    //  and finally a method to open a custom stream
+    virtual Assimp::IOStream *Open(const char *pFile, const char *pMode = "rb") {
+        return new MyIOStream(pFile);
+    }
 
-  void Close( Assimp::IOStream* pFile) { delete pFile; }
+    void Close(Assimp::IOStream *pFile) { delete pFile; }
 };
 
-bool DoTheImportThing( const char* pFile) {
-  // Create an instance of the Importer class
-  Assimp::Importer importer;
-  jslogs((char*)&importer );
-            /*
+bool DoTheImportThing(const char *pFile) {
+    // Create an instance of the Importer class
+    Assimp::Importer importer;
 
-  // And have it read the given file with some example postprocessing
-  // Usually - if speed is not the most important aspect for you - you'll
-  // probably to request more postprocessing than we do in this example.
-  const aiScene* scene = importer.ReadFile( pFile,
-    aiProcess_CalcTangentSpace       |
-    aiProcess_Triangulate            |
-    aiProcess_JoinIdenticalVertices  |
-    aiProcess_SortByPType);
+    // the import process will now use this implementation to access any file
+    // importer.ReadFile( pFile, SomeFlag | SomeOtherFlag);
 
-  // If the import failed, report it
-  if (nullptr != scene) {
-    //DoTheErrorLogging( importer.GetErrorString());
-    return false;
-  }
+    jslogs((char *)&importer);
 
-  // Now we can access the file's contents.
-  //DoTheSceneProcessing( scene);
-*/
-  // We're done. Everything will be cleaned up by the importer destructor
-  return true;
+    // put my custom IO handling in place
+    importer.SetIOHandler(new MyIOSystem());
+
+
+    // And have it read the given file with some example postprocessing
+    // Usually - if speed is not the most important aspect for you - you'll
+    // probably to request more postprocessing than we do in this example.
+    const aiScene *scene = importer.ReadFile(pFile,
+            aiProcess_CalcTangentSpace |
+                    aiProcess_Triangulate |
+                    aiProcess_JoinIdenticalVertices |
+                    aiProcess_SortByPType);
+
+    // If the import failed, report it
+    if (nullptr != scene) {
+        // DoTheErrorLogging( importer.GetErrorString());
+        return false;
+    }
+
+    // Now we can access the file's contents.
+    // DoTheSceneProcessing( scene);
+    // We're done. Everything will be cleaned up by the importer destructor
+    return true;
 }
-
-
-
